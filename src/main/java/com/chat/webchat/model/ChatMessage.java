b5d8089705c085dd614e0de245cb263595f7aee8
신인철 <setia@sin-incheol-ui-MacBookAir.local>
2022-07-10 15:16:10 +0900

    Initial commit

---
+++ src/main/java/com/chat/webchat/model/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.chat.webchat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType type;
+
+    // 채팅방ID
+    private String roomId;
+
+    // 보내는 사람
+    private String sender;
+
+    // 내용
+    private String message;
+}
